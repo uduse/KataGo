efb0a0818a856a7fd2d5acd7f619743159764d01
uduse <i@uduse.com>
2020-06-17 17:32:39 -0600

    Update.

--- cpp/analyze.py
+++ cpp/analyze.py
@@ -1,5 +1,5 @@
 # first, run this command in shell to generate data.
-# $ ./ katago match - config match.cfg -log-file log.log
+# $ ./katago match -config match.cfg -log-file log.log
 # then run this script
 
 from scipy.spatial import distance
@@ -30,7 +30,8 @@ def main():
                 data_raw.append(line)
 
     data = []
-    for line in data_raw[:20000]:
+    # for line in random.sample(data_raw, 20000):
+    for line in tqdm.tqdm(data_raw):
         try:
             data.append(parse(line))
         except AssertionError:
@@ -38,30 +39,44 @@ def main():
 
     print('len(data):', len(data))
 
-    # num_samples = 1000000
-    # xs, ys = [], []
-    # for _ in range(num_samples):
-    #     (l_win, l_own), (r_win, r_own) = random.sample(data, 2)
-    #     similarity = 1 - distance.cosine(l_own, r_own)
-    #     diff = abs(l_win - r_win)
-    #     xs.append(similarity)
-    #     ys.append(diff)
+    corrs = []
+    for _ in range(30):
+        xs, ys = compare_with_random_ones(data)
+        # xs, ys = compare_with_similar_ones(data)
+        # print('samples: ', len(xs))
+        corr = np.corrcoef(xs, ys)[0][1]
+        print(corr)
+        corrs.append(corr)
 
+    print('avr:', sum(corrs) / len(corrs))
+    print('std:', np.std(corrs))
+
+
+def compare_with_random_ones(data):
+    num_samples = 10000
+    xs, ys = [], []
+    for _ in range(num_samples):
+        (l_win, l_own), (r_win, r_own) = random.sample(data, 2)
+        dist = distance.cosine(l_own, r_own)
+        diff = abs(l_win - r_win)
+        xs.append(dist)
+        ys.append(diff)
+    return xs, ys
+
+
+def compare_with_similar_ones(data):
     num_samples = 10000
     xs, ys = [], []
     for _ in tqdm.tqdm(range(num_samples)):
         l_win, l_own = random.choice(data)
-        tops = sorted(random.sample(data, 100), key=lambda x: 1 - distance.cosine(l_own, x[1]))[:5]
+        tops = sorted(random.sample(data, 100), key=lambda x: distance.cosine(l_own, x[1]))[:5]
         for r_win, r_own in tops:
             if abs(l_win - r_win) >= 0.0001:
-                similarity = 1 - distance.cosine(l_own, r_own)
+                dist = distance.cosine(l_own, r_own)
                 diff = abs(l_win - r_win)
-                xs.append(similarity)
+                xs.append(dist)
                 ys.append(diff)
-
-    print(len(xs))
-    # print(ys)
-    print(np.corrcoef(xs, ys))
+    return xs, ys
 
 
 if __name__ == '__main__':
